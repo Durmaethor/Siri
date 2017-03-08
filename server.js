85542e27f09de27623df6e9936790daf48af4b21
Ryan Kramer <rkramer613@gmail.com>
2017-03-07 19:55:10 -0700

    Added messages

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ const app = express();
 app.listen(port, function(){
     console.log(`Listening on port: ${port}`);
 });
+
+const messages = ["H-E-LL-O!", "I'm sorry, you suck!", "The schnozberries taste like schnozberries!", "Napolean, give me some of your tots!", "I'm Batman!", "What about second breakfast?", "You think thats air you are breathing?"]
