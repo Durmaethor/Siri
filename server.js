d35e097ff6aceaf300111f771361d31691a91117
Ryan Kramer <rkramer613@gmail.com>
2017-03-07 20:05:42 -0700

    Tested with Postman

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const messages = ["H-E-LL-O!", "I'm sorry, you suck!", "The schnozberries taste like schnozberries!", "Napolean, give me some of your tots!", "I'm Batman!", "What about second breakfast?", "You think thats air you are breathing?"];
+
+function getRandomMessage () {
+    const randomNum = Math.floor(Math.random() * messages.length);
+    return messages[randomNum];
+}
+
 const express = require('express');
 const port = 8887;
 
@@ -7,4 +14,8 @@ app.listen(port, function(){
     console.log(`Listening on port: ${port}`);
 });
 
-const messages = ["H-E-LL-O!", "I'm sorry, you suck!", "The schnozberries taste like schnozberries!", "Napolean, give me some of your tots!", "I'm Batman!", "What about second breakfast?", "You think thats air you are breathing?"]
+app.get('/', function(req, res){
+    res.send(JSON.stringify({
+        message: getRandomMessage()
+    }))
+});
