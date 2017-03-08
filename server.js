686a98a627ecf58c3e693bca162dfb3d6fd7b7b4
Ryan Kramer <rkramer613@gmail.com>
2017-03-07 20:13:02 -0700

    Added in OPTIONS

--- server.js
+++ server.js
@@ -14,8 +14,26 @@ app.listen(port, function(){
     console.log(`Listening on port: ${port}`);
 });
 
+app.options('/', function(req, res) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send();
+});
+
 app.get('/', function(req, res){
-    res.send(JSON.stringify({
-        message: getRandomMessage()
-    }))
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify({ message: getRandomMessage() }));
 });
